8479a01ff0f3999245eb878b5b329bfd542502ae
Biswarup Chakravarty <biswarup.chakravarty@gmail.com>
2019-10-18 18:35:15 +0200

    Move test data to yml files

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule Snor.MixProject do
     [
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.21", only: :dev},
-      {:earmark, "~> 1.4", only: :dev}
+      {:earmark, "~> 1.4", only: :dev},
+      {:yaml_elixir, "~> 2.4", only: [:dev, :test]}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
---
+++ test/data_test.exs
@@ -0,0 +1,52 @@
+defmodule Snor.DataTest do
+  use ExUnit.Case
+  require YamlElixir
+
+  path = Path.join(File.cwd!(), "test/data/variables.yml")
+
+  case YamlElixir.read_from_file(path) do
+    {:error, error} ->
+      raise error
+
+    {:ok, test_cases} ->
+      test_cases
+      |> Enum.with_index()
+      |> Enum.each(fn {test_case, index} ->
+        @index index
+        @error test_case["error"]
+        @name test_case["name"] || ""
+        @template test_case["template"]
+        @expected_result test_case["result"]
+        @data test_case["data"] || %{}
+
+        case @error do
+          nil ->
+            test "[#{@index}] #{@name}" do
+              actual_result = Snor.render(@template, @data)
+              assert actual_result == @expected_result
+            end
+
+          # test "[COMPILE] [#{@index}] #{@name}" do
+          #    src =
+          #        @template
+          #        |> Snor.Parser.parse()
+          #        |> Snor.Compiler.compile()
+
+          #      compiled_src = Code.string_to_quoted!(src)
+          #      module_name = String.to_atom("CTemplate.#{@index}")
+          #      Module.create(module_name, compiled_src, Macro.Env.location(__ENV__))
+
+          #      actual_result = module_name.execute(@data)
+          #      assert actual_result == @expected_result
+          #    end
+
+          _ ->
+            test "[#{@index}] #{@name}" do
+              assert_raise(String.to_atom("Elixir." <> @error), fn ->
+                Snor.render(@template, @data)
+              end)
+            end
+        end
+      end)
+  end
+end
